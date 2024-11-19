25bb31fff6fca2dfc9eb9b8f4497705da34f7ed2
yigitnurver <yigitnurver643@gmail.com>
2024-11-19 13:48:48 -0500

    this is my second commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String pword="I'm so greatfull";
+        System.out.println("I said hello world my previous session"+pword);
+    }
+}
